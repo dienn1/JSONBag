db80cc585e20e68b7520ea23d7e636b055c5e8a4
dienn1 <dienn1@uci.edu>
2025-07-13 22:16:07 +0100

    Update dominion_const.py

--- const/dominion_const.py
+++ const/dominion_const.py
@@ -1,19 +1,19 @@
 from typing import Literal
 
 features = ['Round', 'Tick', 'CardsInSupplyGameEnd(WORKSHOP)',
-                     'CardsInSupplyGameEnd(GOLD)', 'CardsInSupplyGameEnd(MARKET)',
-                     'CardsInSupplyGameEnd(DUCHY)', 'CardsInSupplyGameEnd(SILVER)',
-                     'CardsInSupplyGameEnd(REMODEL)', 'CardsInSupplyGameEnd(COPPER)',
-                     'CardsInSupplyGameEnd(MERCHANT)', 'CardsInSupplyGameEnd(MILITIA)',
-                     'CardsInSupplyGameEnd(VILLAGE)', 'CardsInSupplyGameEnd(MOAT)',
-                     'CardsInSupplyGameEnd(CELLAR)', 'CardsInSupplyGameEnd(MINE)',
-                     'CardsInSupplyGameEnd(SMITHY)', 'CardsInSupplyGameEnd(PROVINCE)',
-                     'CardsInSupplyGameEnd(ESTATE)', 'EmptySupplySlots(EmptySupplySlots)',
-                     'DominionActionValue(attackCount)', 'DominionActionValue(plusMoney)',
-                     'DominionActionValue(plusActions)',
-                     'DominionActionValue(trashCardCount)', 'DominionActionValue(plusBuys)',
-                     'DominionActionValue(plusDraws)'
-                     ]
+            'CardsInSupplyGameEnd(GOLD)', 'CardsInSupplyGameEnd(MARKET)',
+            'CardsInSupplyGameEnd(DUCHY)', 'CardsInSupplyGameEnd(SILVER)',
+            'CardsInSupplyGameEnd(REMODEL)', 'CardsInSupplyGameEnd(COPPER)',
+            'CardsInSupplyGameEnd(MERCHANT)', 'CardsInSupplyGameEnd(MILITIA)',
+            'CardsInSupplyGameEnd(VILLAGE)', 'CardsInSupplyGameEnd(MOAT)',
+            'CardsInSupplyGameEnd(CELLAR)', 'CardsInSupplyGameEnd(MINE)',
+            'CardsInSupplyGameEnd(SMITHY)', 'CardsInSupplyGameEnd(PROVINCE)',
+            'CardsInSupplyGameEnd(ESTATE)', 'EmptySupplySlots(EmptySupplySlots)',
+            'DominionActionValue(attackCount)', 'DominionActionValue(plusMoney)',
+            'DominionActionValue(plusActions)',
+            'DominionActionValue(trashCardCount)', 'DominionActionValue(plusBuys)',
+            'DominionActionValue(plusDraws)'
+            ]
 
 game_name = "Dominion"
 actual_name = game_name
@@ -37,9 +37,9 @@ seed_count = 4
 tokenize_mode: Literal["both", "ordered", "unordered", "char"] = "unordered"
 
 agent_paths = list(f"{game_name}/agents/{agent_types[i]}/" for i in range(len(agent_types)))
-param_paths = list(f"{game_name}/gameParams/param{i}/" for i in range(1, param_count+1))
-seed_paths = list(f"{game_name}/seeds/seed{i}/" for i in range(1, seed_count+1))
+param_paths = list(f"{game_name}/gameParams/param{i}/" for i in range(1, param_count + 1))
+seed_paths = list(f"{game_name}/seeds/seed{i}/" for i in range(1, seed_count + 1))
 
 agent_dataset = [agent_paths, agent_labels]
-param_dataset = [param_paths, list(f"param{i}" for i in range(1, param_count+1))]
-seed_dataset = [seed_paths, list(f"seed{i}" for i in range(1, seed_count+1))]
+param_dataset = [param_paths, list(f"param{i}" for i in range(1, param_count + 1))]
+seed_dataset = [seed_paths, list(f"seed{i}" for i in range(1, seed_count + 1))]
